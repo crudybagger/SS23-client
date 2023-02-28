d57a9ce2721f8991ab8a5ceea0969bc38fd133d0
crudybagger <sarangnagpal38@gmail.com>
2023-02-28 20:06:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+//react component App
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const sliderRef = useRef(null);
+    const slideLeft = () => {
+        sliderRef.current.scrollLeft -= sliderRef.current.offsetWidth;
+    }
+    const slideRight = () => {
+        sliderRef.current.scrollLeft += sliderRef.current.offsetWidth;
+    }
+    //set the style of each div with class image to have a background image url
+    const slider = sliderRef.current;
+    const images = slider.querySelectorAll('.image');
+    images.forEach(image => {
+        image.style.backgroundImage = `url(https://picsum.photos/500/300?random=${Math.random()})`;
+    });
+
+    return (
+        //Return a react component with class slider, and a div with class slider-inner, which has 5 divs with class image
+        <div className="slider">
+            <div className="slider-inner">
+                <div className="image"></div>
+                <div className="image"></div>
+                <div className="image"></div>
+                <div className="image"></div>
+                <div className="image"></div>
+            </div>
+        </div>
+        // <Router>
+        // <Switch>
+        //     <Route exact path="/" component={Home} />
+        //     <Route path="/about" component={About} />
+        //     <Route path="/contact" component={Contact} />
+        // </Switch>
+        // </Router>
+    );
 }
 
 export default App;
